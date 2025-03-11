23ad8456255ef3b74d9b38edd1c4213b3613874f
tsthuy <huydo282003@gmail.com>
2025-03-11 10:39:30 +0700

    refactor: fix logic lintstaged and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,3 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]],
-    "subject-case": [2, "always", "sentence-case"]
-  }
+  extends: ["@commitlint/config-conventional"]
 }
