e9359d2a50a30e71aeaf0b2b79dc5dc5a2a18b9d
tsthuy <huydo282003@gmail.com>
2025-03-11 09:57:51 +0700

    feat: setup source-base react with vite + typedscript + tailwinscss + Eslint

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button"
+
+export default function App() {
+  return (
+    <>
+      <h1 className='text-red-700'>Hello</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
