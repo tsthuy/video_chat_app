02b8b9b8f6412bf2d6b1a040313bcd7a910b0888
tsthuy <huydo282003@gmail.com>
2025-03-11 13:49:06 +0700

    feat: config env and axios custom

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,10 @@ import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
 
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import checkFile from "eslint-plugin-check-file"
+import unusedImports from "eslint-plugin-unused-imports"
+
 export default tseslint.config(
   { ignores: ["dist", "vite.config.ts"] },
   {
@@ -18,7 +22,10 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      prettier: eslintPluginPrettier
+      prettier: eslintPluginPrettier,
+      "simple-import-sort": simpleImportSort,
+      "check-file": checkFile,
+      "unused-imports": unusedImports
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -36,7 +43,50 @@ export default tseslint.config(
           printWidth: 120,
           jsxSingleQuote: true
         }
+      ],
+      "no-var": "error",
+      "prefer-const": "error",
+      "simple-import-sort/imports": "error",
+      "check-file/filename-naming-convention": ["error", { "**/*.{ts,tsx}": "KEBAB_CASE" }],
+      "check-file/folder-naming-convention": ["error", { "src/**/*": "KEBAB_CASE" }],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
       ]
+    }
+  },
+
+  {
+    files: ["**/{hooks,utils,services,components,constants,libs,types,stores,mocks}/*.ts"],
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/hooks/**/*.ts": "use-**.hook",
+          "**/utils/**/*.ts": "**.util",
+          "**/services/**/*.ts": "**.service",
+          "**/components/**/*.ts": "**.component",
+          "**/constants/**/*.ts": "**.const",
+          "**/libs/**/*.ts": "**.lib",
+          "**/types/**/*.ts": "**.d",
+          "**/stores/**/*.ts": "**.store",
+          "**/mocks/**/*.ts": "**.mock"
+        }
+      ]
+    }
+  },
+
+  {
+    files: ["src/**/index.ts", "src/**/App.tsx"],
+    rules: {
+      "check-file/filename-naming-convention": "off",
+      "check-file/folder-naming-convention": "off"
     }
   }
 )
---
+++ src/services/config.service.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios"
+
+interface ApiClientOptions {
+  baseURL: string
+  headers?: Record<string, string>
+  timeout?: number
+  customInterceptors?: {
+    request?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig
+    response?: (response: AxiosResponse) => AxiosResponse
+    error?: (error: any) => Promise<any>
+  }
+}
+
+class ApiClient {
+  private instance: AxiosInstance
+
+  constructor(options: ApiClientOptions) {
+    const { baseURL, headers = { "Content-Type": "application/json" }, timeout = 10000, customInterceptors } = options
+
+    this.instance = axios.create({
+      baseURL,
+      headers,
+      timeout
+    })
+
+    this.instance.interceptors.request.use(
+      (config: InternalAxiosRequestConfig) => {
+        let locale: string
+
+        if (typeof window !== "undefined") {
+          locale = localStorage.getItem("locale") || "en"
+        } else {
+          locale = (config as any).locale || "en"
+        }
+
+        config.headers["Accept-Language"] = locale
+
+        if (customInterceptors?.request) {
+          config = customInterceptors.request(config)
+        }
+
+        return config
+      },
+      (error) => Promise.reject(error)
+    )
+
+    this.instance.interceptors.response.use(
+      (response) => {
+        return customInterceptors?.response ? customInterceptors.response(response) : response
+      },
+      (error) => {
+        if (error.response?.status === 401) {
+          console.error("Unauthorized access - redirecting to login.")
+        }
+        return customInterceptors?.error ? customInterceptors.error(error) : Promise.reject(error)
+      }
+    )
+  }
+
+  async get<T>(url: string, config?: AxiosRequestConfig & { locale?: string }): Promise<T> {
+    const response = await this.instance.get<T>(url, config)
+    return response.data
+  }
+
+  async post<T>(url: string, data?: any, config?: AxiosRequestConfig & { locale?: string }): Promise<T> {
+    const response = await this.instance.post<T>(url, data, config)
+    return response.data
+  }
+
+  async put<T>(url: string, data?: any, config?: AxiosRequestConfig & { locale?: string }): Promise<T> {
+    const response = await this.instance.put<T>(url, data, config)
+    return response.data
+  }
+
+  async delete<T>(url: string, config?: AxiosRequestConfig & { locale?: string }): Promise<T> {
+    const response = await this.instance.delete<T>(url, config)
+    return response.data
+  }
+}
+
+const defaultApiClient = new ApiClient({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "",
+  headers: { "Content-Type": "application/json" },
+  timeout: 10000
+})
+
+const storeApiClient = new ApiClient({
+  baseURL: process.env.NEXT_PUBLIC_STORE_API_URL || "",
+  headers: { "Content-Type": "application/json" },
+  timeout: 10000
+})
+
+export { defaultApiClient, storeApiClient }
