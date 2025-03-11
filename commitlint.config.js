f49acab459c747057d9db91d9439746f7f770882
tsthuy <huydo282003@gmail.com>
2025-03-11 10:19:14 +0700

    feat: setup husky, lint-staged, commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]],
+    "subject-case": [2, "always", "sentence-case"]
+  }
+}
